91dd97a37a2ac1976a69c6da330f4186509f0b3c
sangram-samanta <sangram@suyogindia.com>
2024-03-04 18:48:00 +0530

    changes

--- src/Screens/Dashboard/index.tsx
+++ src/Screens/Dashboard/index.tsx
@@ -1,5 +1,5 @@
 import {View, Text, FlatList, Linking, Pressable, Image} from 'react-native';
-import React, { useState, useSyncExternalStore } from 'react';
+import React, { useEffect, useState, useSyncExternalStore } from 'react';
 import {orderlist} from '../../Constans/Constant';
 import BodyWarpper from '../../Components/BodyWarpper';
 import {styles} from './StyleSheet';
@@ -8,12 +8,52 @@ import Icons from '../../Components/Icons';
 import { useNavigation } from '@react-navigation/native';
 import { NativeStackNavigationProp } from '@react-navigation/native-stack';
 import { RootStackParamList } from '../../Navigation/RootStackPrams';
+import AsyncStorage from '@react-native-async-storage/async-storage';
+import { CallApi } from '../../CallApi';
+import Loader from '../../Components/Loader';
 type DashboardScreenProp = NativeStackNavigationProp<RootStackParamList, 'Dashboard'>;
 const Dashboard = () => {
   const [filtervalue , setFiltervalue] = useState('Today')
+  const [loading, setLoading] = useState(false);
+  const [filtershow, setFiltershow] = useState(false);
+  const [orderData, setOrderData] = useState(false);
   const navigation=useNavigation<DashboardScreenProp>()
+  useEffect(()=>{
+    getData()
+  },[])
+  const getData = async () => {
+    const value = await AsyncStorage.getItem('access_token');
+    const requestData =JSON.stringify(
+     {
+      request_type: 'customer_booking_lists',
+      token: value,
+      itemsPerPage:10,
+      currentPage:1,
+      start_date:"2024-03-02",
+      end_date:"2024-03-02"
+      // driver_mobile_number: '8018364674',
+    })
+    setLoading(true);
+console.log(requestData)
+    try {
+      const response = await CallApi('POST','manage_driver',requestData);
+      console.log(response,'myresponse--->');
+      if (response.success === 1) {
+        setOrderData(response.data)
+        setLoading(false);
+      }else{
+        setLoading(false);
+        console.log('server error'); 
+      }
+      
+    } catch (error) {
+      setLoading(false);
+      console.error('There was an error!', error);
+    }
+  };
   return (
     <BodyWarpper>
+             {loading&& <Loader loading={loading} />}
       <View style={{ width: '100%',}}>
 
      
@@ -33,17 +73,23 @@ const Dashboard = () => {
           <Pressable style={{width:'43%',marginLeft:'2%'}}onPress={()=>navigation.navigate('Profile')}>
           <Text>Lingaraj Sahoo</Text>
           </Pressable>
-          <Pressable style={{width:'25%',backgroundColor:'orange', height:40,justifyContent:'center',alignItems:'center',borderRadius:5}}>
+          <Pressable style={{width:'25%',backgroundColor:'orange', height:40,justifyContent:'center',alignItems:'center',borderRadius:5}} onPress={()=>setFiltershow(!filtershow)}>
           <Text>Filter</Text>
           </Pressable>
        
-          <View style={{width:'15%',height:40,justifyContent:'center',alignItems:'flex-end'}}>
-          <Text>ddd</Text>
-          </View>
+          <Pressable style={{width:'15%',height:40,justifyContent:'center',alignItems:'flex-end'}}>
+          {/* <Text>ddd</Text> */}
+          <Icons
+          type="FontAwesome"
+          name="bell"
+          color={Colors.Primary}
+          size={25}
+        /> 
+          </Pressable>
         </View>
        
       </View>
-      <View style={styles.filterDropDown}>
+      {filtershow&& <View style={styles.filterDropDown}>
         <Pressable style={{...styles.filterbtn,backgroundColor:filtervalue==='Today'?Colors.green:Colors.white,}} onPress={()=>setFiltervalue('Today')}>
         <Text style={{...styles.filterbtntext,color:filtervalue==='Today'?Colors.white:Colors.black}}>Today</Text>
         </Pressable>
@@ -58,10 +104,10 @@ const Dashboard = () => {
           <Text style={{...styles.filterbtntext,color:filtervalue==='Date Range'?Colors.white:Colors.black}}>Date Range</Text>
           </Pressable>
 
-         </View>
+         </View>}
       </View>
       <FlatList
-        data={orderlist}
+        data={orderData}
         keyExtractor={(item, index) => 'key' + index}
         contentContainerStyle={{paddingBottom: 40}}
         renderItem={({item, index}) => {
